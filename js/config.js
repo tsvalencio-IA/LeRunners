b318485428cdac6983384898498746e5a03ca3c1
tsvalencio-IA <tsvalencio@gmail.com>
2025-11-13 12:25:04 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,28 +1,16 @@
 // js/config.js
-// COLE AQUI AS CHAVES QUE VOCÊ COPIOU DO PASSO 1.3 (Fase 1)
+// COLE AQUI AS CHAVES DO PASSO 4 DA CONFIGURAÇÃO DO FIREBASE
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAfu5pWp01QwkCAtuX3XvVT686cojlS7g4",
-    authDomain: "lerunners-app.firebaseapp.com",
-    databaseURL: "https://lerunners-app-default-rtdb.firebaseio.com",
-    projectId: "lerunners-app",
-    storageBucket: "lerunners-app.firebasestorage.app",
-    messagingSenderId: "341500611045",
-    appId: "1:341500611045:web:634f56e09d21ef9e40070c"
+    apiKey: "AIzaSyDEfyw4v2UlVw85swueLoEnGjYY95xh2NI",
+    authDomain: "lerunners-a6de2.firebaseapp.com",
+    databaseURL: "https://lerunners-a6de2-default-rtdb.firebaseio.com",
+    projectId: "lerunners-a6de2",
+    storageBucket: "lerunners-a6de2.firebasestorage.app",
+    messagingSenderId: "24483751716",
+    appId: "1:24483751716:web:313b3013bd11c75e2eb5b1"
   };
 
 // --- Configuração da GOOGLE GEMINI API (MÓDULO 3 - FUTURO) ---
-// NÃO PODE SER USADA NO GITHUB PAGES (INSEGURO)
-// Crie em: https://aistudio.google.com/app/apikey
+// AVISO: Não é seguro usar no GitHub Pages. Precisará de um back-end.
 // const GEMINI_API_KEY = "AIzaSy...SUA_CHAVE_GEMINI";
-
-
-// --- Configuração do Strava (MÓDULO 2 - FUTURO) ---
-// NÃO PODE SER USADA NO GITHUB PAGES (INSEGURO)
-/*
-const stravaConfig = {
-    clientId: "SEU_CLIENT_ID_DO_STRAVA",
-    clientSecret: "SEU_CLIENT_SECRET_DO_STRAVA", // <--- ESTA CHAVE NÃO PODE FICAR PÚBLICA
-    redirectUri: "URL_DO_SEU_BACKEND_DE_AUTENTICACAO" 
-};
-*/
