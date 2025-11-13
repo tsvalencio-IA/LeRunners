a3a1ba782eea4fb0fee035035ac12d40d95e33cd
tsvalencio-IA <tsvalencio@gmail.com>
2025-11-13 13:18:07 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,14 +2,14 @@
 // COLE AQUI AS CHAVES DO PASSO 4 DA CONFIGURAÇÃO DO FIREBASE
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDEfyw4v2UlVw85swueLoEnGjYY95xh2NI",
-    authDomain: "lerunners-a6de2.firebaseapp.com",
-    databaseURL: "https://lerunners-a6de2-default-rtdb.firebaseio.com",
-    projectId: "lerunners-a6de2",
-    storageBucket: "lerunners-a6de2.firebasestorage.app",
-    messagingSenderId: "24483751716",
-    appId: "1:24483751716:web:313b3013bd11c75e2eb5b1"
-  };
+  apiKey: "AIzaSy...COLE_SUA_CHAVE_AQUI",
+  authDomain: "lerunners-plataforma.firebaseapp.com",
+  databaseURL: "https://lerunners-plataforma-default-rtdb.firebaseio.com",
+  projectId: "lerunners-plataforma",
+  storageBucket: "lerunners-plataforma.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
 
 // --- Configuração da GOOGLE GEMINI API (MÓDULO 3 - FUTURO) ---
 // AVISO: Não é seguro usar no GitHub Pages. Precisará de um back-end.
